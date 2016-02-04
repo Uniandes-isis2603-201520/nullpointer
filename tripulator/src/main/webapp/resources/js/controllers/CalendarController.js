5aaa266f43dab9157d7075379eb916fd14c2d6e6
Antonio de la Vega <antoniodlavga@gmail.com>
2016-02-04 02:00:00 -0500

    Entire new layout given to calendar.html. CalendarController modified to support changes.

--- tripulator/src/main/webapp/resources/js/controllers/CalendarController.js
+++ tripulator/src/main/webapp/resources/js/controllers/CalendarController.js
@@ -1,28 +1,62 @@
 app.controller('CalendarController', ['$scope', 'dayInformationService', function ($scope, dayInformationService) {
-        var dayinms = parseInt(1000*60*60*24,10,10);
         var totdays = [];
-        var start = parseInt(dayInformationService.init,10);
-        var end = parseInt(dayInformationService.end,10);
-        var npages = -1;
+        totdays[0] = [];
+        //new Date(dayInformationService.init) | 
+        //new Date(dayInformationService.end) |
+        var startDate = new Date("2015-03-24");
+        var endDate =  new Date("2015-04-10");
+        var npages = 0;
         var nitems = 0;
-        for(var i = start; i <= end; i+=dayinms, nitems++){
-            var index = nitems%12;
-            if(index === 0)  {
-                npages++;
-                totdays[npages] = [];
+
+        var monthStart = new Date(startDate.getTime());
+        var currentMonth = monthStart.getMonth();
+        
+        monthStart.setDate(1);
+        
+        for(var i = monthStart; i <= endDate; i.setDate(i.getDate() + 1), nitems++){
+
+            if(currentMonth !== i.getMonth())  {
+                currentMonth = i.getMonth();
+                nitems=0;
+                totdays[++npages] = [];
             }
-            totdays[npages][index] = i;
+            totdays[npages][nitems] = {
+                date: i.getTime(),
+                valid: ((i-startDate) > 0)
+            };
         }
 
-        $scope.init = start;
-        $scope.fin = end;
         $scope.days = totdays;
         $scope.pages = npages;
         $scope.pagenum = 0;
+        
         $scope.increasePageNum = function(){
             $scope.pagenum++;
         };
         $scope.decreasePageNum = function(){
             $scope.pagenum--;
+        };
+        
+        $scope.squareBackground = function(day, highlight){
+            if(!day.valid && !highlight){
+                return{
+                    "background-color" : "rgba(94, 168, 167, 0.5)"
+                };
+            }
+            else if(!day.valid && highlight){
+                return{
+                    "background-color" : "rgba(94, 168, 167, 1)"
+                };
+            }
+            else if(day.valid && !highlight){
+                return{
+                    "background-color": "rgba(180, 209, 255, 0.5)"
+                };
+            }
+            if(day.valid && highlight){
+                return{
+                    "background-color": "rgba(180, 209, 255, 1)"
+                };
+            }
         };
 }]);
