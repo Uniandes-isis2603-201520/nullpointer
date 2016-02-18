03f39d2a477589f08dced5b844aacb8ed1fd8a7e
Daniel Delgado <da.delgado11@uniandes.edu.co>
2016-02-17 20:13:26 -0500

    Various changes to Site UI and Angular improvements.

--- tripulator/src/main/webapp/src/modules/viajero/controllers/ViajeroController.js
+++ tripulator/src/main/webapp/src/modules/viajero/controllers/ViajeroController.js
@@ -1,35 +1,35 @@
-app.controller('HomeController', ['$scope','dayInformationService', function ($scope, dayInformationService) {
-        $scope.setValue = function(value){
-          dayInformationService.init += value;  
-          alert(dayInformationService.init);
+app.controller('HomeController', ['$scope', 'dayInformationService', function ($scope, dayInformationService) {
+        $scope.setValue = function (value) {
+            dayInformationService.init += value;
+            alert(dayInformationService.init);
         };
-        $scope.setInit = function(value){
-          dayInformationService.init = value;  
-          alert(dayInformationService.init);
+        $scope.setInit = function (value) {
+            dayInformationService.init = value;
+            alert(dayInformationService.init);
         };
-        $scope.setEnd = function(value){
-          dayInformationService.end = value;  
-          alert(dayInformationService.end);
+        $scope.setEnd = function (value) {
+            dayInformationService.end = value;
+            alert(dayInformationService.end);
         };
         
-}]);
-
-/**app.controller('MainController', ['$scope', function($scope) {
-$scope.viajes = [
-  { 
-    image: 'http://digital.fespa.com/images/Amsterdam.jpg', 
-    nombre: 'Amsterdam', 
-    Fecha: '17/05/1996'
-  }, 
-  { 
-    image: 'http://media-cdn.tripadvisor.com/media/photo-s/07/bd/a1/8a/downtown-dubai-cityscape.jpg', 
-    nombre: 'Dubai',  
-    fecha: '11/22/2005' 
-  },
-  ]
-  }]);*/
-
-angular.module('calendarioAgregar',
-    ['ngMaterial', 'ngMessages']).controller('AppCtrl', function($scope) {
-  $scope.myDate = new Date();
-});
+        $scope.myDate = new Date();
+        
+        $scope.viajes = [
+       /** {
+        image: 'http://digital.fespa.com/images/Amsterdam.jpg',
+                nombre: 'Amsterdam',
+                Fecha: '17/05/1996'
+        },
+        {
+        image: 'http://media-cdn.tripadvisor.com/media/photo-s/07/bd/a1/8a/downtown-dubai-cityscape.jpg',
+                nombre: 'Dubai',
+                fecha: '11/22/2005'
+        },
+        {
+        image: 'https://i.imgur.com/6EUAQg4.png',
+                nombre: 'PlaceHolder',
+                fecha: 'DatePlaceHolder'
+        },*/
+        ]
+        
+    }]);
