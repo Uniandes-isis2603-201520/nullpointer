350db665cc589353146d66917c8ced83915b4252
Daniel Delgado <da.delgado11@uniandes.edu.co>
2016-02-25 10:09:33 -0500

    Fleshed out past Additions

--- tripulator/src/main/webapp/src/modules/viajero/services/viajeroS.svc.js
+++ tripulator/src/main/webapp/src/modules/viajero/services/viajeroS.svc.js
@@ -1,6 +1,8 @@
 (function (ng) {
     var mod = ng.module("viajeroModule");
     mod.service('viajeroS', [function () {
+            
+            
             this.getValue1 = function () {
                 return this.myValue1;
             };
@@ -16,5 +18,38 @@
             this.setValue2 = function (newValue) {
                 this.myValue2 = newValue;
             };
+            
+        /**
+        this.fetchViajes = function () {
+            return $http.get(context);
+        };
+
+
+        this.irViaje = function (id) {
+            return $http.get(context + "/" + id);
+        };
+
+
+        this.saveViaje = function (currentRecord) {
+            if (currentRecord.id) {
+                return $http.put(context + "/" + record.id, record);
+            } else {
+                return $http.post(context, record);
+            }
+        };
+
+        this.deleteViaje = function (id) {
+            return $http.delete(context + "/" + id);
+        };  
+
+        this.getViaje = function (id) {
+            return $http.get(context + "/" + id + "/viajes");
+        };
+
+
+        this.removeViaje = function (authorId, bookId) {
+            return $http.delete(context + "/viajes/" + viajesId);
+        };
+        */
         }]);
 })(window.angular);
