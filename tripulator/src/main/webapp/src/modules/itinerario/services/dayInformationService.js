926ef1ad6e3e976c3f1dbae358aa7f3005013323
Antonio de la Vega <antoniodlavga@gmail.com>
2016-02-24 21:10:31 -0500

    Added use of promises to itinerario service and itinerario controller.

--- tripulator/src/main/webapp/src/modules/itinerario/services/dayInformationService.js
+++
@@ -1,9 +0,0 @@
-(function (ng) {
-    var mod = ng.module("itinerarioModule");
-    mod.service('dayInformationService', [function () {
-            return{
-                init: 0,
-                end: 0
-            };
-        }]);
-})(window.angular);
