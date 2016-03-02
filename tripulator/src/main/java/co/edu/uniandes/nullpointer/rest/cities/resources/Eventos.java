c05ac813ab6084d2981e4ba141814bd3a1f1d127
unknown <jd.fandino10@uniandes.edu.co>
2016-03-02 06:03:32 -0500

    dependencies

---
+++ tripulator/src/main/java/co/edu/uniandes/nullpointer/rest/cities/resources/Eventos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.nullpointer.rest.cities.resources;
+
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author jd.fandino10
+ */
+@Path("eventos")
+public class Eventos {
+
+}
