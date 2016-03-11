4d723007a844a9dd4f499ba3cbb9d5a89f7ce28b
Jose Daniel <jd.fandino10@uniandes.edu.co>
2016-03-11 18:48:52 -0500

    avance rest evento

--- tripulator/src/main/java/co/edu/uniandes/nullpointer/rest/tripulator/dtos/EventoDTO.java
+++ tripulator/src/main/java/co/edu/uniandes/nullpointer/rest/tripulator/dtos/EventoDTO.java
@@ -7,16 +7,156 @@ package co.edu.uniandes.nullpointer.rest.tripulator.dtos;
 
 /**
  *
- * @author Antonio de la Vega
+ * @author Jose Daniel
  */
 public class EventoDTO {
 
-    public Long getId() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static final String tipoE ="Evento";
+    public static final String tipoS ="Sitio";
+    
+    private Long id;//Long para el identificador del evento
+    private String title; //String del nombre del evento
+    private String image; //String de la ruta de la imagen del evento
+    private String type;  //String del tipo del evento (sitio o evento)
+    private Long start;  //Long de la fecha de inicio
+    private Long end;    //Long de la fecha final
+    private String description;   //String de la descripcion
+    //comments;  //Arreglo de comentarios
+
+    /**
+     * Constructor por defecto
+     */
+    public EventoDTO() {
     }
 
-    public void setId(long newId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    /**
+     * Constructor con parámetros.
+     *
+     * @param id identificador del evento
+     * @param title nombre del evento
+     * @param image ruta de la imagen del evento
+     * @param type tipo del evento
+     * @param start long defecha inicio del evento
+     * @param end long de fecha fin del evento
+     * @param description descripcion del evento
+     */
+    public EventoDTO(Long id, String title, String image, String type, Long start, Long end, String description) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.type = type;
+        this.start = start;
+        this.end = end;
+        this.description = description;
     }
-    
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the image
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the start
+     */
+    public Long getStart() {
+        return start;
+    }
+
+    /**
+     * @param start the start to set
+     */
+    public void setStart(Long start) {
+        this.start = start;
+    }
+
+    /**
+     * @return the end
+     */
+    public Long getEnd() {
+        return end;
+    }
+
+    /**
+     * @param end the end to set
+     */
+    public void setEnd(Long end) {
+        this.end = end;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Convierte el objeto a una cadena
+     */
+    @Override
+    public String toString() {
+        return "{id:" + id + ",\n title:'" + title + "',\n image:'" + image
+                +"', \n type:'" + type + "',\n start:" + start + ",\n  end:"
+                + end + ",\n description:'" + description + "', }";
+    }
+
 }
---
+++ tripulator/src/main/java/co/edu/uniandes/nullpointer/rest/tripulator/resources/EventoResource.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.nullpointer.rest.tripulator.resources;
+
+import co.edu.uniandes.nullpointer.rest.tripulator.dtos.EventoDTO;
+import co.edu.uniandes.nullpointer.rest.tripulator.exceptions.TripulatorLogicException;
+import co.edu.uniandes.nullpointer.rest.tripulator.mocks.EventoLogicMock;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+/**
+ *
+ * @author jd.fandino10
+ */
+@Path("eventos")
+public class EventoResource {
+
+    
+	@Inject
+	EventoLogicMock eventoLogic;
+
+	/**
+	 * Obtiene el listado de eventos. 
+	 * @return lista de eventos
+	 * @throws TripulatorLogicException excepción retornada por la lógica  
+	 */
+    @GET
+    public List<EventoDTO> getEventos() throws TripulatorLogicException {
+        return eventoLogic.getEventos();
+    }
+
+    /**
+     * Obtiene un evento
+     * @param id identificador de el evento
+     * @return evento encontrado
+     * @throws TripulatorLogicException cuando el evento no existe
+     */
+    @GET
+    @Path("{id: \\d+}")
+    public EventoDTO getEvento(@PathParam("id") Long id) throws TripulatorLogicException {
+        return eventoLogic.getEvento(id);
+    }
+
+    /**
+     * Agrega un evento
+     * @param Evento evento a agregar
+     * @return datos de el evento a agregar
+     * @throws TripulatorLogicException cuando ya existe un evento con el id suministrado
+     */
+    @POST
+    public EventoDTO createEvento(EventoDTO Evento) throws TripulatorLogicException {
+        return eventoLogic.createEvento(Evento);
+    }
+
+    /**
+     * Actualiza los datos de un evento
+     * @param id identificador de el evento a modificar
+     * @param Evento evento a modificar
+     * @return datos de el evento modificada 
+     * @throws TripulatorLogicException cuando no existe un evento con el id suministrado
+     */
+    @PUT
+    @Path("{id: \\d+}")
+    public EventoDTO updateEvento(@PathParam("id") Long id, EventoDTO Evento) throws TripulatorLogicException {
+        return eventoLogic.updateEvento(id, Evento);
+    }
+
+    /**
+     * Elimina los datos de un evento
+     * @param id identificador de el evento a eliminar
+     * @throws TripulatorLogicException cuando no existe un evento con el id suministrado
+     */
+    @DELETE
+    @Path("{id: \\d+}")
+    public void deleteEvento(@PathParam("id") Long id) throws TripulatorLogicException {
+    	eventoLogic.deleteEvento(id);
+    }
+}
