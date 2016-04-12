a0cd0932bca50173676f3fcbce456b30896d754e
Antonio de la Vega <antoniodlavga@gmail.com>
2016-04-12 11:19:35 -0500

    Added ItinerarioPersistenceTest.

---
+++ tripulator-logic/src/test/java/co/edu/uniandes/csw/tripulator/persistence/ItinerarioPersistenceTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.tripulator.persistence;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author Antonio de la Vega
+ */
+@RunWith(Arquillian.class)
+public class ItinerarioPersistenceTest {
+    
+    public ItinerarioPersistenceTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
