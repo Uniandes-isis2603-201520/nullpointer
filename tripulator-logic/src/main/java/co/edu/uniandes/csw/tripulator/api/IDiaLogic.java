ebf7dd05da0470ca2210a6e028e2ef74025c5259
n.gomez22 <n.gomez22@172.24.100.214>
2016-04-20 22:44:30 -0500

    added DiaLogicTests, fixed DiaLogic

--- tripulator-logic/src/main/java/co/edu/uniandes/csw/tripulator/api/IDiaLogic.java
+++ tripulator-logic/src/main/java/co/edu/uniandes/csw/tripulator/api/IDiaLogic.java
@@ -1,5 +1,6 @@
 package co.edu.uniandes.csw.tripulator.api;
 import co.edu.uniandes.csw.tripulator.entities.DiaEntity;
+import co.edu.uniandes.csw.tripulator.entities.EventoEntity;
 import co.edu.uniandes.csw.tripulator.exceptions.BusinessLogicException;
 import java.util.List;
 
@@ -7,11 +8,21 @@ public interface IDiaLogic {
 
     public List<DiaEntity> getDias(Long idViajero, Long idItinerario);
 
-    public DiaEntity getDia(Long idViajero, Long idItinerario, Long id) throws BusinessLogicException;
+    public DiaEntity getDia(Long idViajero, Long idItinerario, Long idDia) throws BusinessLogicException;
 
     public DiaEntity createDia(Long idViajero, Long idItinerario, DiaEntity dia);
 
     public DiaEntity updateDia(Long idViajero, Long idItinerario, DiaEntity dia);
 
-    public void deleteDia(Long idViajero, Long idItinerario, Long id);
+    public void deleteDia(Long idViajero, Long idItinerario, Long idDia);
+    
+    public List<EventoEntity> getEventos(Long idViajero, Long idItinerario, Long idDia);
+    
+    public EventoEntity getEvento(Long idViajero, Long idItinerario, Long idDia, Long idEvento);
+    
+    public EventoEntity addEvento(Long idViajero, Long idItinerario, Long idDia, Long idEvento);
+    
+    public void removeEvento(Long idViajero, Long idItinerario, Long idDia, Long idEvento);
+   
+    public List<EventoEntity> replaceEventos(Long idViajero, Long idItinerario, Long idDia, List<EventoEntity>eventos);
 }
